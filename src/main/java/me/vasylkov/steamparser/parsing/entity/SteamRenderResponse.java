6e0c2ec1e426c733eeaf3112b843e9097b8d652b
Anton Vasylkov <neatrabe345@gmail.com>
2024-09-16 10:04:37 +0200

    Create Jackson entities for CSFloatAPI

--- src/main/java/me/vasylkov/steamparser/parsing/entity/SteamRenderResponse.java
+++ src/main/java/me/vasylkov/steamparser/parsing/entity/SteamRenderResponse.java
@@ -2,7 +2,10 @@ package me.vasylkov.steamparser.parsing.entity;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.util.List;
 import java.util.Map;
 
 @Data
@@ -14,4 +17,50 @@ public class SteamRenderResponse
     private int pageSize;
     private int totalCount;
     private Map<String, Map<String, Map<String, SteamRenderAssetLink>>> assets;
+
+    @Setter
+    public static class SteamRenderAssetLink
+    {
+        private String id;
+        @Getter
+        private String link;
+
+        public SteamRenderAssetLink()
+        {
+        }
+
+        public SteamRenderAssetLink(String id, String link)
+        {
+            this.id = id;
+            this.link = link;
+        }
+
+        @JsonProperty("id")
+        public String getId()
+        {
+            return id;
+        }
+
+        @JsonProperty("actions")
+        private void unpackNested(List<Action> actions)
+        {
+            if (actions != null && !actions.isEmpty())
+            {
+                this.link = actions.get(0).getLink().replace("%assetid%", this.id);
+            }
+        }
+
+        @Setter
+        public static class Action
+        {
+            private String link;
+
+            @JsonProperty("link")
+            public String getLink()
+            {
+                return link;
+            }
+
+        }
+    }
 }
